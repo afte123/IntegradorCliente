71b0ccc566d1681292f6501695208768011ef26a
afte123 <afte-2222@hotmail.com>
2016-03-08 21:29:38 -0500

    Agregar  clase de comunicación y tarea.... Comienzo del código del login y registro... Uso del intent para cambiar de pantallas

--- app/src/main/java/login/integradorcliente/Login.java
+++ app/src/main/java/login/integradorcliente/Login.java
@@ -1,23 +1,50 @@
 package login.integradorcliente;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
 
+public class Login extends AppCompatActivity implements Observer {
+EditText nombre, contra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nombre=(EditText)findViewById(R.id.nombre);
+        contra = (EditText)findViewById(R.id.contrasena);
+
+        Comunicacion.getInstance().setJefe(this);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
+
+
+
+    }
+
+    public void bLogin(View v){
+
+    }
+
+    public void bRegistro(View v){
+        Intent intent = new Intent(this, Registro.class);
+      startActivity(intent);
     }
 
     @Override
@@ -33,5 +60,11 @@ public class Login extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void update(Observable observable, Object data) {
+
     }
 }
